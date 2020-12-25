d288c88696b7083009493f299b400a572ad4a89e
Evgenii Zakharov <eugene.zakharov93@gmail.com>
2020-12-25 23:04:03 +0300

    add domain with student and course

---
+++ Domain.fs
@@ -0,0 +1,35 @@
+﻿module Domain
+
+open System
+
+type StudentId = StudentId of int
+type AdmissionYear = AdmissionYear of DateTime
+type ReleaseYear = ReleaseYear of DateTime
+type StudentAge = private StudentAge of int
+
+module StudentAge =
+    let private minimumAge = 15
+    let private maximumAge = 120
+
+    let create age =
+        if age < minimumAge
+        then printf "StudentAge must be greater than %i" minimumAge |> Error
+        else if age > maximumAge
+        then printf "StudentAge must be less than %i" maximumAge |> Error
+        else StudentAge age |> Ok
+
+type CourseId = CourseId of int
+
+type Student =
+    { Id: StudentId
+      FirstName: string
+      LastName: string
+      Age: StudentAge
+      CourseId: CourseId
+      AdmissionYear: AdmissionYear
+      ReleaseYear: ReleaseYear }
+
+type Course =
+    { Id: CourseId
+      Name: string
+      StudentIds: StudentId list }
